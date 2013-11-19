e2a3e4132220a3172e5d0a566e0c225a8c2c8ba4
Nick Jennings <nick@silverbucket.net>
2013-11-19 16:47:36 +0100

    update tools script to make sure master is synced to remote before the gh-pages update

--- tools/update-demo.sh
+++ tools/update-demo.sh
@@ -4,6 +4,7 @@ mkdir ../tmp
 mkdir ../tmp/webfinger.js
 cp demo/index.html ../tmp/webfinger.js/index.html &&
 cp src/webfinger.js ../tmp/webfinger.js/webfinger.js &&
+git push &&
 git checkout gh-pages &&
 cp ../tmp/webfinger.js/index.html demo/ &&
 cp ../tmp/webfinger.js/webfinger.js src/ &&
