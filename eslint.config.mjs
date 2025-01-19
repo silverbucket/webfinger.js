11d77916a1e06ecac87e4648ecd2c08e712afb84
Nick Jennings <nick@silverbucket.net>
2025-01-19 18:05:32 +0100

    WIP refactoring to TS

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    ignores: ["test/**"],
+  },
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
