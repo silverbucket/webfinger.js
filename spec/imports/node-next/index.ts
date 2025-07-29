2d52dc5747ece96b39b47cd4406e3f11fe47894b
Nick Jennings <nick@silverbucket.net>
2025-07-29 19:57:31 +0200

    fix spec

--- spec/imports/node-next/index.ts
+++ spec/imports/node-next/index.ts
@@ -1,12 +1,10 @@
 import WebFinger from "webfinger.js";
 
 new WebFinger({
-  webfist_fallback: false,
   tls_only: true,
 });
 
 new WebFinger.default({
-  webfist_fallback: false,
   tls_only: true,
 });
 
